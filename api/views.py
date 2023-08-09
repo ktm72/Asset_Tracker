6f5e598576e37fe8a81f5fd3b07c636535a7cba2
ktm72 <khondaker.tm@gmail.com>
2023-08-10 00:13:01 +0600

    CRUD for company records
    	See the API section in readme to test different rest api

--- api/views.py
+++ api/views.py
@@ -1,3 +1,5 @@
+from rest_framework import status
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -22,9 +24,39 @@ def company(request):
         serializer = CompanySerializer(companies, many=True)
         return Response(serializer.data)
 
-    if request.method == 'POST':
+    elif request.method == 'POST':
         serializer = CompanySerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors)
+
+
+@api_view(['GET', 'PATCH', 'DELETE'])
+def company_detail(request, company_id):
+    if request.method == 'GET':
+        try:
+            company = Company.objects.get(id=company_id)
+            serializer = CompanySerializer(company, many=False)
+            return Response(serializer.data)
+        except Company.DoesNotExist:
+            return Response({"error": "Company not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    elif request.method == 'PATCH':
+        data = request.data
+        company = Company.objects.get(id=company_id)
+        serializer = CompanySerializer(company, data=data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors)
+
+    elif request.method == 'DELETE':
+        try:
+            company = Company.objects.get(id=company_id)
+            company.delete()
+            return Response({"message": "Company %s deleted!" % company_id}, status=status.HTTP_200_OK)
+        except Company.DoesNotExist:
+            return Response({"error": "Company not exist!"}, status=status.HTTP_204_NO_CONTENT)
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Install Pipenv
+
+`pip install pipenv`
+
+## Install Dependencies
+
+`pipenv install`
+
+## Activate virtualenv
+
+`pipenv shell`
+
+## Deactivate project
+
+`ctrl+D or exit`
+
+# API
+
+- localhost:8000/
+  - api/test -> test purpose
+  - api/company ->GET(show all), POST(entry a company)
+  - api/company/1 ->GET(company details), PATCH(update company), DELETE(delete a company)
